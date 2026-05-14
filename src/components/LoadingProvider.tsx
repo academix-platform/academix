df11f758d3e640b8b2a3465c65ade51ea57a51f7
Abdullah Abusharekh <aboodmabusharekh1412004@gmail.com>
2026-05-14 23:25:05 +0300

    fix: fix admin dashboard styles + loading provider

--- src/components/LoadingProvider.tsx
+++ src/components/LoadingProvider.tsx
@@ -22,16 +22,18 @@ export function LoadingProvider({ children }: { children: ReactNode }) {
     >
       {children}
       {isPending && (
-        <div className="z-50 fixed inset-0 flex justify-center items-center bg-black/20 backdrop-blur-[2px]">
+        <div className="z-50 fixed inset-0 flex justify-center items-center bg-black/10 backdrop-blur-[2px]">
           <div className="flex flex-col items-center gap-5">
-            <div className="flex justify-center items-center bg-white shadow-lg rounded-3xl w-16 sm:w-24 h-16 sm:h-24">
-              <img
-                src="/icon.png"
-                alt="Loading..."
-                className="w-10 sm:w-14 h-10 sm:h-14 animate-wiggle"
-                width={56}
-                height={56}
-              />
+            <div className="bg-gradient-to-br from-[#C026D3] to-[#7C3AED] p-[4px] rounded-full">
+              <div className="flex justify-center items-center bg-white rounded-full w-16 sm:w-24 h-16 sm:h-24">
+                <img
+                  src="/icon.png"
+                  alt="Loading..."
+                  className="w-10 sm:w-14 h-10 sm:h-14 animate-wiggle"
+                  width={56}
+                  height={56}
+                />
+              </div>
             </div>
           </div>
         </div>
